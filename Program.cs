9e11586783123a4be5fc6c2cc83ef10a9b2287cc
Alex Thacker <thacker.alex@yahoo.com>
2019-05-28 11:30:10 -0500

    exercise done

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace planet_and_spaceships
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> planetList = new List<string>(){
+                "Mercury", "Mars"
+                };
+            planetList.Add("Jupitar");
+            planetList.Add("Saturn");
+            
+            // foreach(string planet in planetList) {
+            //     Console.WriteLine(planet);
+            // }
+
+            List <string> lastTwo = new List <string> () {
+                "Uranus", "Neptune"
+            };
+
+            planetList.AddRange(lastTwo);
+            
+            planetList.Insert(1, "Venus");
+
+            planetList.Insert(2, "Earth");
+
+            planetList.Add("Pluto");
+
+            List<string> rockyPlanets = planetList.GetRange(2, 4);
+
+            planetList.Remove("Pluto");
+
+            foreach(string planet in planetList) {
+                Console.WriteLine(planet);
+            }
+        }
+    }
+}
